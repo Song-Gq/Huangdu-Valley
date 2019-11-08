273ee6ed76c268eb97237e8edf2b5554e8435b6d
LeePaangSang <462889098@qq.com>
2019-11-08 14:55:23 +0800

    Update FactoryTest.java

--- src/huangduValley/workshop/test/FactoryTest.java
+++ src/huangduValley/workshop/test/FactoryTest.java
@@ -1,20 +1,20 @@
-/*
-package huangduValley.Workshop.test;
 
-import huangduValley.Workshop.ConcreteMachine.CleanMachine;
-import huangduValley.Workshop.ConcreteMachine.FineProMachine;
-import huangduValley.Workshop.ConcreteMachine.RoughProMachine;
-import huangduValley.Workshop.Machine;
-import huangduValley.Workshop.Product.Product;
-import huangduValley.Workshop.WareHouse.Store;
-import huangduValley.Workshop.WareHouse.Wallet;
-import huangduValley.Workshop.WareHouse.WareHouse;
-import huangduValley.Workshop.processFactory.factory.ClosedFactory;
-import huangduValley.Workshop.processFactory.factory.Context;
-import huangduValley.Workshop.processFactory.factory.FactoryState;
-import huangduValley.Workshop.processFactory.factory.NormalFactory;
-import huangduValley.Workshop.processFactory.workSpace.CompleteProcessing;
-import huangduValley.Workshop.processFactory.workSpace.WorkSpace;
+package huangduValley.workshop.test;
+
+import huangduValley.workshop.concreteMachine.CleanMachine;
+import huangduValley.workshop.concreteMachine.FineProMachine;
+import huangduValley.workshop.concreteMachine.RoughProMachine;
+import huangduValley.workshop.Machine;
+import huangduValley.workshop.product.Product;
+import huangduValley.workshop.wareHouse.Store;
+import huangduValley.workshop.wareHouse.Wallet;
+import huangduValley.workshop.wareHouse.WareHouse;
+import huangduValley.workshop.processFactory.factory.ClosedFactory;
+import huangduValley.workshop.processFactory.factory.Context;
+import huangduValley.workshop.processFactory.factory.FactoryState;
+import huangduValley.workshop.processFactory.factory.NormalFactory;
+import huangduValley.workshop.processFactory.workSpace.CompleteProcessing;
+import huangduValley.workshop.processFactory.workSpace.WorkSpace;
 import huangduValley.farm.storage.Ingredients;
 import huangduValley.farm.storage.Items;
 
@@ -23,7 +23,6 @@ import java.util.Vector;
 public class FactoryTest {
 
     public static void main(String[] args) throws Exception {
-	// write your code here
         Context context=new Context();
         FactoryState factory=ClosedFactory.getInstance();
         context.setState(factory);    //设置状态为工厂关闭
@@ -31,38 +30,37 @@ public class FactoryTest {
         factory= NormalFactory.getInstance();
         context.setState(factory);    //设置状态为工厂开启
 
-        */
-/**
-         * Create machine
-         *//*
+        /**
+        Create machine
+         */
 
         CleanMachine cm = new CleanMachine("1st CM");
         CleanMachine cm2 = (CleanMachine) cm.clone();
         cm2.setName("2nd CM");
         RoughProMachine rm = new RoughProMachine("1st rm");
         FineProMachine fm = new FineProMachine("1st fm");
-        */
-/**
+
+        /**
          * Set wareHouse size
-         *//*
+         */
 
         WareHouse wareHouse = WareHouse.getInstance();
         Store store = Store.getInstance();
         Wallet wallet = Wallet.getInstance();
 
         wareHouse.setMaxSize(10);
-        */
-/**
+
+        /**
          * Create ingredients
-         *//*
+         */
 
         Vector<Items> ingredientsVector = new Vector<>();
         ingredientsVector.add(new Ingredients("rice", 10));
         ingredientsVector.add(new Ingredients("potato", 20));
-        */
-/**
+
+        /**
          * Create processing
-         *//*
+         */
 
         Vector<WorkSpace> processVector = new Vector<>();
         CompleteProcessing processing1 = new CompleteProcessing();
@@ -72,13 +70,13 @@ public class FactoryTest {
         processVector.add(processing1);
         ((NormalFactory) factory).setProcessVector(processVector);
 
-        */
-/**
+
+        /**
          * Run
-         *//*
+         */
 
         context.runState();         //状态为开启工厂下的运行函数
-        Vector<Items> vector1 = wareHouse.getProductsVector().elementAt(0);
+        Vector<Product> vector1 = wareHouse.getProductsVector().elementAt(0);
         System.out.println("Name is " + vector1.elementAt(0).getName());
         System.out.println("Count is " + vector1.elementAt(0).getCount());
         store.buy("rice");
@@ -87,4 +85,3 @@ public class FactoryTest {
         System.out.println(wallet.getBalance());
     }
 }
-*/
